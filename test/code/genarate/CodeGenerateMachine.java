f731670afb698b25036fa76b0f1f3019aa6bb340
martian001 <995396426@qq.com>
2016-07-05 17:50:46 +0800

    增加aom代码生成类

--- test/code/genarate/CodeGenerateMachine.java
+++ test/code/genarate/CodeGenerateMachine.java
@@ -31,25 +31,27 @@ public class CodeGenerateMachine {
    @Test
    public void codeGenerate() throws SQLException {
       try {
-         String srcPath = "C:/Users/Administrator/git/SpringMVC_Spring_MyBatis/src/";
-         String mapperPath = "C:/Users/Administrator/git/SpringMVC_Spring_MyBatis/mapper/";
+         // String srcPath = "C:/Users/Administrator/git/SpringMVC_Spring_MyBatis/src/";
+         String srcPath = "D:/eclipse-jee-luna-R-win32-x86_64/eclipse_workspace/com.xlkfinance.bms/com.xlkfinance.bms.server/src/main/java/";
+         // String mapperPath = "C:/Users/Administrator/git/SpringMVC_Spring_MyBatis/mapper/";
+         String mapperPath = "D:/eclipse-jee-luna-R-win32-x86_64/eclipse_workspace/com.xlkfinance.bms/com.xlkfinance.bms.server/src/main/resources/mapper/aom/";
          List<String> tempLates = new ArrayList<>();
-//         tempLates.add("MapperTempLate.ftl-Mapper");
-//         tempLates.add("ServiceTempLate.ftl-Service");
-//         tempLates.add("ServiceImplTempLate.ftl-Service");
-//         tempLates.add("BeanTempLate.ftl-Bean");
-//         tempLates.add("ActionTempLate.ftl-Action");
-         tempLates.add("MapperXmlTempLate.ftl-Mapper");
+//         tempLates.add("MapperTempLate2.ftl-Mapper");
+         // tempLates.add("ServiceTempLate.ftl-Service");
+         // tempLates.add("ServiceImplTempLate.ftl-Service");
+         // tempLates.add("BeanTempLate.ftl-Bean");
+         // tempLates.add("ActionTempLate.ftl-Action");
+         tempLates.add("MapperXmlTempLate2.ftl-Mapper");
 
          List<Map<String, Object>> list = new ArrayList<>();
-         String name = "CheckingIn";//类名
-         String lowercaseBeanName = "checkingIn";//类名小写
-         String dateTime = DateUtils.getCurrentDateTime();//当前时间
-         String version = "1.0";//版本号
-         String classAnnotation = "系统用户";//类注释
-         String packageModule = "checkingIn";//模块
-         String packageRoot = "com/et/";//包根路径
-         String tableName = "CHECKING_IN";//数据库表名
+         String tableName = "ORG_ASSETS_COOPERATION_INFO";// 数据库表名
+         String name = MapperXmlTempLateUtil.jdbcToJavaName2(tableName);// 类名
+         String lowercaseBeanName = MapperXmlTempLateUtil.startLowerCase(name);// 类名小写
+         String dateTime = DateUtils.getCurrentDateTime();// 当前时间
+         String version = "1.0";// 版本号
+         String classAnnotation = "机构管理平台-资产合作信息";// 类注释
+         String packageModule = "org";// 模块
+         String packageRoot = "com/qfang/xk/aom/server/org/";// 包根路径
 
          for (String tempLate : tempLates) {
             Map<String, Object> map = new HashMap<String, Object>();
@@ -77,7 +79,7 @@ public class CodeGenerateMachine {
          for (Map<String, Object> m : list) {
             String ftlPath = (String) m.get("tempLate");
             String tempLate = (String) m.get("tempLate");
-            if ("MapperXmlTempLate.ftl".equals(tempLate)) {
+            if ("MapperXmlTempLate2.ftl".equals(tempLate)) {
                m.put("fieldMaps", MapperXmlTempLateUtil.getMetaData(tableName));
             }
             String templateParsing = CommomUtils.templateParsing(m, ftlPath);
@@ -92,13 +94,82 @@ public class CodeGenerateMachine {
             if ("BeanTempLate.ftl".equals(tempLate)) {
                fileName = (m.get("beanName").toString()) + ".java";
             }
-            if ("MapperXmlTempLate.ftl".equals(tempLate)) {
-               folderPath = mapperPath + m.get("packageModule").toString()+File.separator;
+            if ("MapperXmlTempLate2.ftl".equals(tempLate)) {
+               folderPath = mapperPath + m.get("packageModule").toString() + File.separator;
                fileName = (m.get("beanName").toString()) + "Mapper.xml";
             }
             System.out.println(folderPath);
             System.out.println(fileName);
-            FileUtils.textToFile(templateParsing, folderPath, fileName);
+             FileUtils.textToFile(templateParsing, folderPath, fileName);
+         }
+      } catch (TemplateException e) {
+         e.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+
+   @Test
+   public void codeGenerate2() throws SQLException {
+      try {
+         String srcPath = "D:/eclipse-jee-luna-R-win32-x86_64/eclipse_workspace/com.xlkfinance.bms/com.xlkfinance.bms.server/src/main/java/";
+         String mapperPath = "D:/eclipse-jee-luna-R-win32-x86_64/eclipse_workspace/com.xlkfinance.bms/com.xlkfinance.bms.server/src/main/resources/mapper/aom/";
+         List<String> tempLates = new ArrayList<>();
+//         tempLates.add("ServiceImplTempLate2.ftl-Service");
+//         tempLates.add("MapperTempLate2.ftl-Mapper");
+//         tempLates.add("MapperXmlTempLate2.ftl-Mapper");
+//         tempLates.add("TestTempLate.ftl-Test");
+         tempLates.add("ThriftTempLate.ftl-Test");
+
+         List<Map<String, Object>> list = new ArrayList<>();
+         String tableName = "ORG_USER_INFO";// 数据库表名
+         String name = MapperXmlTempLateUtil.jdbcToJavaName2(tableName);// 类名
+         String lowercaseBeanName = MapperXmlTempLateUtil.startLowerCase(name);// 类名小写
+         String dateTime = DateUtils.getCurrentDateTime();// 当前时间
+         String version = "1.0";// 版本号
+         String classAnnotation = "机构管理平台-资产合作信息";// 类注释
+         String packageModule = "org";// 模块
+         String packageRoot = "com/qfang/xk/aom/server/";// 包根路径
+
+         for (String tempLate : tempLates) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            String packageLayer = tempLate.substring(tempLate.lastIndexOf("-") + 1);
+            tempLate = tempLate.substring(0, tempLate.lastIndexOf("-"));
+            map.put("tempLate", tempLate);
+            if ("ServiceImplTempLate2.ftl".equals(tempLate)) {
+               map.put("className", name + packageLayer + "Impl");
+            } else {
+               map.put("className", name + packageLayer);
+            }
+            map.put("beanName", name);
+            map.put("lowercaseBeanName", lowercaseBeanName);
+            map.put("dateTime", dateTime);
+            map.put("version", version);
+            map.put("classAnnotation", classAnnotation);
+            map.put("packageModule", packageModule);// 包-模块
+            map.put("packageLayer", packageLayer.toLowerCase());// 包，层级（action,service,mapper）
+            map.put("tableName", tableName);
+            list.add(map);
+         }
+         for (Map<String, Object> m : list) {
+            String ftlPath = (String) m.get("tempLate");
+            String tempLate = (String) m.get("tempLate");
+            m.put("fieldMaps", MapperXmlTempLateUtil.getMetaData(tableName));
+            String templateParsing = CommomUtils.templateParsing(m, ftlPath);
+            System.out.println(templateParsing);
+            String folderPath = srcPath + packageRoot + m.get("packageModule").toString() + File.separator
+                  + m.get("packageLayer").toString().replace(".", File.separator) + File.separator;
+            if ("ServiceImplTempLate2.ftl".equals(tempLate)) {
+               folderPath = srcPath + packageRoot + m.get("packageModule").toString() + "/service/";
+            }
+            String fileName = (m.get("className").toString()) + ".java";
+            if ("MapperXmlTempLate2.ftl".equals(tempLate)) {
+               folderPath = mapperPath + m.get("packageModule").toString() + File.separator;
+               fileName = (m.get("beanName").toString()) + "Mapper.xml";
+            }
+            System.out.println(folderPath);
+            System.out.println(fileName);
+//             FileUtils.textToFile(templateParsing, folderPath, fileName);
          }
       } catch (TemplateException e) {
          e.printStackTrace();
--- test/code/genarate/MapperXmlTempLateUtil.java
+++ test/code/genarate/MapperXmlTempLateUtil.java
@@ -26,7 +26,7 @@ import com.et.util.CommomUtils;
  * ★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★<br>
  */
 public class MapperXmlTempLateUtil {
-   private final static String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+   private final static String URL = "jdbc:oracle:thin:@192.168.0.212:1521:orcl";
    private final static String DRIVER = "oracle.jdbc.driver.OracleDriver";
    private static Connection conn = null;
 
@@ -62,7 +62,7 @@ public class MapperXmlTempLateUtil {
    static {// 静态代码块.
       try {
          Class.forName(DRIVER);
-         conn = DriverManager.getConnection(URL, "oadb", "Ab123456");// url,用户名和密码
+         conn = DriverManager.getConnection(URL, "bms_bak", "Ab123456");// url,用户名和密码
       } catch (Exception e) {
          e.printStackTrace();
       }
@@ -75,11 +75,11 @@ public class MapperXmlTempLateUtil {
    @Test
    public void testMetaData() throws Exception {
       Map<String, Object> m = new HashMap<String, Object>();
-      String tableName = "CHECKING_IN";
+      String tableName = "ORG_USER_INFO";
       m.put("tableName", tableName);
-      m.put("beanName", "CheckingIn");
-      m.put("packageModule", "checkingIn");
-      m.put("lowercaseBeanName", "checkingIn");
+      m.put("beanName", jdbcToJavaName2(tableName));
+      m.put("packageModule", "system");
+      m.put("lowercaseBeanName", jdbcToJavaName(tableName));
 
       List<Map<String, String>> maps = getMetaData(tableName);
       // for (Map<String, String> fieldMap : maps) {
@@ -90,7 +90,7 @@ public class MapperXmlTempLateUtil {
       // System.out.println(javaName + "(" + javaType + ") -----------" + jdbcName + "(" + jdbcType + ")");
       // }
       m.put("fieldMaps", maps);
-      String templateParsing = CommomUtils.templateParsing(m, "MapperXmlTempLate.ftl");
+      String templateParsing = CommomUtils.templateParsing(m, "MapperXmlTempLate2.ftl");
       System.out.println(templateParsing);
    }
 
@@ -111,16 +111,7 @@ public class MapperXmlTempLateUtil {
          String jdbcName = rsmd.getColumnName(i);// 表的字段名.
          String jdbcType = rsmd.getColumnTypeName(i);// jdbc字段类型
          String javaType = TYPE_MAP.get(jdbcType);// java字段类型
-         StringBuffer javaName = new StringBuffer();// java字段名
-         String[] words = jdbcName.toLowerCase().split("_");// 表的字段名转成小写并且分割成单词
-         for (int j = 0; j < words.length; j++) {
-            String word = words[j];// 获取单词
-            if (j > 0) {
-               javaName.append(captureName(word));
-               continue;
-            }
-            javaName.append(word);
-         }
+         String javaName = jdbcToJavaName(jdbcName);// java字段名
          rs.next();
          Map<String, String> fieldMap = new HashMap<>();
          fieldMap.put("jdbcName", jdbcName);
@@ -132,6 +123,29 @@ public class MapperXmlTempLateUtil {
       return maps;
    }
 
+   public static String jdbcToJavaName(String jdbcName) {
+      StringBuffer javaName = new StringBuffer();
+      String[] words = jdbcName.toLowerCase().split("_");// 表的字段名转成小写并且分割成单词
+      for (int j = 0; j < words.length; j++) {
+         String word = words[j];// 获取单词
+         if (j > 0) {
+            javaName.append(startUpperCase(word));
+            continue;
+         }
+         javaName.append(word);
+      }
+      return javaName.toString();
+   }
+   public static String jdbcToJavaName2(String jdbcName) {
+      StringBuffer javaName = new StringBuffer();
+      String[] words = jdbcName.toLowerCase().split("_");// 表的字段名转成小写并且分割成单词
+      for (int j = 0; j < words.length; j++) {
+         String word = words[j];// 获取单词
+         javaName.append(startUpperCase(word));
+      }
+      return javaName.toString();
+   }
+
    /**
     * 首字母大写方法
     *@author:liangyanjun
@@ -139,8 +153,12 @@ public class MapperXmlTempLateUtil {
     *@param name
     *@return
     */
-   public static String captureName(String name) {
+   public static String startUpperCase(String name) {
       name = name.substring(0, 1).toUpperCase() + name.substring(1);
+      return name;
+   }
+   public static String startLowerCase(String name) {
+      name = name.substring(0, 1).toLowerCase() + name.substring(1);
       return name;
    }
 
